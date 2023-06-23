ca96520ccacbc12e86c5bcae8043b6cf8f52d137
waqasalamzeb <79015762+waqasalamzeb@users.noreply.github.com>
2023-06-23 13:23:49 -0700

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function Submit(){
     const password = document.getElementById("password").value
     const allUsers = JSON.parse(localStorage.getItem("users"));
     
-    //use find loop for best approach
+//use find loop for best approach
     for(i = 0; i < allUsers.length; i++){
         if(allUsers[i].email == email && allUsers[i].password == password){
             localStorage.setItem("activeUser", JSON.stringify({email, password, username: allUsers[i].username}))
